673c9632484e4d7b304459eccb829486ab29f4c4
frankmayala072 <frankmayala072@gmail.com>
2026-08-11 15:15:54 +0300

    feat:creat user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post ,HttpCode,HttpStatus} from '@nestjs/common';
+
+import { UsersService } from './users.service';
+import { createUserDto } from './dto/create-user.dto';
+import { updateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: createUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: updateUserDto) {
+    return this.usersService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(id);
+  }
+}   
+
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
+import {randomUUID} from 'crypto';
+import {createUserDto} from './dto/create-user.dto';
+import {updateUserDto} from './dto/update-user.dto';
+import {User} from './entities/user.entity';    
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [];
+  
+    create(createUserDto: createUserDto) {
+      const user: User = {
+        id: randomUUID(),
+        ...createUserDto,
+        password: (createUserDto as any).password,
+        createdAt: new Date(),
+      };
+      this.users.push(user);
+      return user;
+    }
+     
+    findAll(): User[] {
+      return this.users;
+    }
+
+    findOne(id: string): User {
+      const user = this.users.find((user) => user.id === id);   
+      if (!user) {
+        throw new NotFoundException(`User with ID ${id} not found`);
+      }
+      return user;
+    }
+
+    updtae(id: string, updateUserDto: updateUserDto): User {
+      const user = this.findOne(id);
+      Object.assign(user, updateUserDto);
+      return user;
+    }
+
+    remove(id: string): void {
+      const index = this.users.findIndex((user) => user.id === id);
+      if (index === -1) {
+        throw new NotFoundException(`User with ID ${id} not found`);
+      }
+      this.users.splice(index, 1);
+    }
+
+}
+
+
