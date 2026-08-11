704e83885a4497d4086c9b398dbbe6254ef43b94
Abdulrazaq333 <abdulrazaqabdallah2005@gmail.com>
2026-08-11 17:22:36 +0300

    feat: implement authentication module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+/*import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+     TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'your_username', // Replace with teammate's Postgres username
+      password: 'your_password', // Replace with teammate's Postgres password
+      database: 'your_database', // Replace with teammate's database name
+      entities: [User],
+      synchronize: true, // Auto-create tables (dev only)
+    }),
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}*/
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [AuthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
