1bb9530dd8be82208df88bc0ee2075ce076542fb
batholomeo syoko <batholomeosyoko@gmail.com>
2026-08-13 12:11:30 +0300

    fix: resolve corrupted merge markers in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
- HEAD
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-
- version1
 
 @Module({
   imports: [
@@ -20,15 +19,12 @@ import { UsersModule } from './users/users.module';
         password: config.get('DB_PASSWORD'),
         database: config.get('DB_NAME'),
         autoLoadEntities: true,
- HEAD
-        synchronize: true,
-      }),
-    }),
-    UsersModule,
         synchronize: true, // dev only
       }),
     }),
- version1
+    UsersModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
